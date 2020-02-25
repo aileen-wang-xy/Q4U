441947712cf9fd7fd6cb750434487b0a6e6c5f3c
aileen-wang-xy <aileen-wang-xy@gmail.com>
2020-02-25 11:24:41 +0000

    authentication almost done, excepte authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 
 
-    def after_sign_in_path_for(resource)
-        redirect_to static_pages_home_path
-    end
+    # def after_sign_in_path_for(resource)
+    #     redirect_to 'static_pages/home'
+    # end
 
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -30,5 +30,15 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    
+    user ||= User.new # guest user (not logged in)
+    if user.superadmin_role?
+      can :manage, :all
+      can :access, :rails_admin       # only allow admin users to access Rails Admin
+      can :manage, :dashboard         # allow access to dashboard
+    end
+    if user.supervisor_role?
+      can :manage, User
+    end
   end
 end
