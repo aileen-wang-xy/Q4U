6304821ffe8b6fb8c19a9cf945f2e6b04f7043ac
aileen-wang-xy <aileen-wang-xy@gmail.com>
2020-03-25 19:09:14 +0000

    add user_id to comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_213109) do
+ActiveRecord::Schema.define(version: 2020_03_25_161641) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_22_213109) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id"
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
