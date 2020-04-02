4be1bad00b9bfeef39b3709474d0097bbcfb4007
aileen-wang-xy <aileen-wang-xy@gmail.com>
2020-04-02 15:54:41 +0100

    finish profile page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,13 @@ Rails.application.routes.draw do
     resources :deals 
     resources :reviews
   end
+
+  # devise_for :users, :skip => [:registrations] 
+  # as :user do
+  #   get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+  #   patch 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  # end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "static_pages#home"
   post 'deals/create', to: 'deals#create'
