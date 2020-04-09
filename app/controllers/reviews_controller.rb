be26183d25ff98406261d24c9dc0459ee46ceb50
aileen-wang-xy <aileen-wang-xy@gmail.com>
2020-04-09 22:05:47 +0100

    clean review file, delete edit page, delete update, edit and destroy method in controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,10 +24,6 @@ class ReviewsController < ApplicationController
     #---------------------------------------------
   end
 
-  # GET /reviews/1/edit
-  def edit
-  end
-
   # POST /reviews
   # POST /reviews.json
   def create
@@ -61,30 +57,6 @@ class ReviewsController < ApplicationController
         format.html { render :new }
         format.json { render json: @review.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
-  def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
-  def destroy
-    @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
