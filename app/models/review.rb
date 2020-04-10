6d86591d7cf581f45f796b8a26e00805f09c6a4a
aileen-wang-xy <aileen-wang-xy@gmail.com>
2020-04-10 11:40:58 +0100

    add validates to reivew

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :reviewer, class_name: 'User'
   belongs_to :be_reviewed, class_name: 'User'
   belongs_to :deal
+  validates :rating, presence: true
 end
