a006dafd2bcb80731bcfb6f54dd6b1f4f24bd5ba
aileen-wang-xy <aileen-wang-xy@gmail.com>
2020-04-20 23:19:23 +0100

    home page html

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-    # @requests = Post.where(service_type: request).order("created_at DESC").limit(8)
+    # @request = Post.where(service_type: request).order("created_at DESC").limit(8)
     # @provides = Post.where(service_type: provide).order("created_at DESC").limit(8)
+    @posts = Post.all.order("created_at DESC").limit(8)
   end
 
   def account
   end
 
-  def search
-  end
 end
